70681ae789f3aa299df7ef3fa5e47353aefce7d5
yutoBeat <90952921+yutoBeat@users.noreply.github.com>
2025-03-02 02:37:49 +1300

    text replace and more booster

--- cards/booster.lua
+++ cards/booster.lua
@@ -5,39 +5,41 @@ SMODS.Atlas {
     py = 95
 }
 
-local jokers = {
-    'j_feet_mirko_ice',
-    'j_feet_pearl',
-    'Marina_Meat',
-    'Pearls_meal',
-    'j_feet_ghost',
-    'Doki',
-    'j_feet_dandandan',
-    'j_fuet_splatoon',
-    'j_feet_pantyhose',
-    'j_feet_Tripple',
-    'j_feet_Cum',
-    'j_feet_Steamly',
-    'j_feet_Roboco',
-    'j_futa__IrYs',
-    'dokianal',
-    'j_futa_chart',
-    'j_futa_splatdance',
-    'j_feet_Lana',
-    'j_feet_Peko',
-    'j_feet_Twins',
-    'j_feet_selen',
-    'j_feet_marie',
-    'j_feet_pantyhosefeet',
-    'j_feet_dp',
-    'j_feet_Fromuppper',
-    'j_futa_tori',
-    'j_feet_towa',
-    'j_feet_botan',
-    'j_feet_footlick',
-    'j_feet_frog smell',
-    'j_futa_gardivoir'
+local joker_keys = {
+    "j_feet_mirko_ice",
+    "j_feet_pearl",
+    "Marina_Meat",
+    "Pearls_meal",
+    "j_feet_ghost",
+    "Doki",
+    "j_feet_dandandan",
+    "j_fuet_splatoon",
+    "j_feet_pantyhose",
+    "j_feet_Tripple",
+    "j_feet_Cum",
+    "j_feet_Steamly",
+    "j_feet_Roboco",
+    "j_futa__IrYs",
+    "dokianal",
+    "j_futa_chart",
+    "j_futa_splatdance",
+    "j_feet_Lana",
+    "j_feet_Peko",
+    "j_feet_Twins",
+    "j_feet_selen",
+    "j_feet_marie",
+    "j_feet_pantyhosefeet",
+    "j_feet_dp",
+    "j_feet_Fromuppper",
+    "j_futa_tori",
+    "j_feet_towa",
+    "j_futa_splat_orgy",
+    "j_feet_footlick",
+    "j_feet_frog smell",
+    "j_futa_gardivoir",
+    "j_foot_collection"
 }
+
 
 local function getrand_futajoker()
     local futajokers = {}
@@ -66,9 +68,6 @@ local footjoker = {}
         return "j_bery_" .. jokerkey  
     end
 end
-
- test
-
 
 SMODS.Booster {
     key = 'booster_Futa', 
@@ -106,6 +105,54 @@ SMODS.Booster {
         text = {
             "A step in the right direction.", 
             "Choose up to {C:attention}1{} of {C:attention}3{} Foot Themed Jokers"
+        }, 
+        group_name = "Choose a Foot",
+    }, 
+    create_card = function(self, card, i)
+        local new_card = SMODS.create_card{
+            set = 'Joker',
+            key = getrand_footjoker()
+        }
+        return new_card
+    end
+}
+
+SMODS.Booster {
+    key = 'booster_Futa_Large', 
+    atlas = 'booster',
+    pos = {x = 2, y = 0},
+    weight = 10, 
+    cost = 6, 
+    config = {extra = 4, choose = 2},
+    loc_txt = {
+        name = 'Futa Mega Pack',
+        text = {
+            "A larger bag of {C:attention}Girl-dicks{}", 
+            "Choose up to {C:attention}2{} of {C:attention}4{} Futa Themed Jokers"
+        }, 
+        group_name = "Choose a Futa",
+    }, 
+    create_card = function(self, card, i)
+        local new_card = SMODS.create_card{
+            set = 'Joker',
+            key = getrand_futajoker()
+        }
+        return new_card
+    end
+}
+
+SMODS.Booster {
+    key = 'booster_Foot_Large', 
+    atlas = 'booster',
+    pos = {x = 3, y = 0},
+    weight = 10, 
+    cost = 6, 
+    config = {extra = 5, choose = 2},
+    loc_txt = {
+        name = 'The Sole Mega Collection',
+        text = {
+            "A larger step in the right direction.", 
+            "Choose up to {C:attention}2{} of {C:attention}5{} Foot Themed Jokers"
         }, 
         group_name = "Choose a Foot",
     }, 
